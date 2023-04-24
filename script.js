d522b1b85e3d1068c60410dbeb2526710a1ee313
Adir <slutzkis@gmail.com>
2023-04-24 16:15:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var x = document.querySelectorAll("a");
 var i;
 for (i = 0; i < x.length; i++) {
     x[i].addEventListener("click", function(){
-        alert(x[i].href);
+        alert(this.href);
     });
 }

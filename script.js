bc2084e386c5d580c56928dfb07ff508d220955b
Adir <slutzkis@gmail.com>
2023-04-24 16:16:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var x = document.querySelectorAll("a");
 var i;
 for (i = 0; i < x.length; i++) {
     x[i].addEventListener("click", function(){
-        alert(this.href);
+        // alert(this.href);
+        window.history.pushState("", "", this.href);
     });
 }

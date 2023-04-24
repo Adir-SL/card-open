1c64914312ccc86a24c03f83733fc841b64f26df
Adir <slutzkis@gmail.com>
2023-04-24 16:10:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ var Anchors = document.getElementsByTagName("a");
 for (var i = 0; i < Anchors.length ; i++) {
     Anchors[i].addEventListener("click", 
         function (event) {
-            event.preventDefault();
-            if (confirm('Are you sure?')) {
-                window.location = this.href;
-            }
+            // event.preventDefault();
+            window.history.pushState("","", Anchors[i].href);
         }, 
         false);
 }
